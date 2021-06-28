742e28259cdd97e8010dfada7dd3af603834435b
TeGameofLife <85854214+zhangqian0003172@users.noreply.github.com>
2021-06-28 08:55:07 +0800

    Update and rename src/main/java/com/example/springboot/hello.java to 源文件/主要的/爪哇/电脑/例子/弹簧靴/hello.java

--- 源文件/主要的/爪哇/电脑/例子/弹簧靴/hello.java
+++ 源文件/主要的/爪哇/电脑/例子/弹簧靴/hello.java
@@ -3,5 +3,8 @@ package com.example.springboot;
 public class hello {
     public static void main(String[] args) {
         System.out.println("11111");
+ System.out.println("11111");
+ System.out.println("11111");
+ System.out.println("11111"); System.out.println("11111");
     }
 }
