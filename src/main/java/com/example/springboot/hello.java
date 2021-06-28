55dcfaa70712cfa53f3d77f02897142c800993a4
GameLift <3376229910@qq.com>
2021-06-28 09:05:56 +0800

    000

---
+++ src/main/java/com/example/springboot/hello.java
@@ -0,0 +1,10 @@
+package com.example.springboot;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+    }
+}
