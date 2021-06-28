02d256f89998845ac8a9acbd9bc20cc9b0e95e59
TeGameofLife <85854214+zhangqian0003172@users.noreply.github.com>
2021-06-28 09:12:46 +0800

    Update hello.java

--- src/main/java/com/example/springboot/hello.java
+++ src/main/java/com/example/springboot/hello.java
@@ -6,5 +6,7 @@ public class hello {
         System.out.println("1111");
         System.out.println("1111");
         System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("update");
     }
 }
